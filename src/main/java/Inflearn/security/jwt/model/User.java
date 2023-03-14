18484ef21377902e1dfd39bb8bea3980cb347eeb
Yang <didtdjwns111@gmail.com>
2023-03-14 12:14:26 +0900

    feat: User Entity 생성

---
+++ src/main/java/Inflearn/security/jwt/model/User.java
@@ -0,0 +1,32 @@
+package Inflearn.security.jwt.model;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String roles;
+
+    public List<String> getRoleList() {
+        if (!this.roles.isEmpty()) {
+            return Arrays.asList(this.roles.split(","));
+        }
+        return new ArrayList<>();
+    }
+}
