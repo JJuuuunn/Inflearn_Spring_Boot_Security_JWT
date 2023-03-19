acdbe75bc6226edc1ab4883fc68b99099a4d7ef5
Yang <didtdjwns111@gmail.com>
2023-03-19 21:26:47 +0900

    feat: BCryptPasswordEncoder 추가
    페스워드 암호화 하기 위해 추가

--- src/main/java/Inflearn/security/jwt/config/SecurityConfig.java
+++ src/main/java/Inflearn/security/jwt/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package Inflearn.security.jwt.config;
 
+import Inflearn.security.jwt.config.jwt.JwtAuthenticationFilter;
 import Inflearn.security.jwt.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.web.filter.CorsFilter;
 
@@ -17,10 +20,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CorsFilter corsFilter;
 
+    // Password Encoding 메소드
+    @Bean
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class);
-        http.csrf().disable();
+//        http.addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class);
+        http.csrf().disable(); // 토큰 사용 방식을 위해 csrf 비활성화
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // (STATELESS) 세션을 사용하지 않게 설정
                 .and()
                 .addFilter(corsFilter) // @CrossOrigin(인증X), 시큐리티 필터에 등록 인증(O)
@@ -31,6 +40,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/v1/manager/**").access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
                 .antMatchers("/api/v1/admin/**").access("hasRole('ROLE_ADMIN')")
                 .anyRequest().permitAll()
-                ;
+        ;
     }
 }
