49aef71bd95946cad06792da628c8ce3a3410cf9
Yang <didtdjwns111@gmail.com>
2023-03-19 21:25:39 +0900

    feat: join api 생성
    회원가입 기능 생성

--- src/main/java/Inflearn/security/jwt/controller/RestApiController.java
+++ src/main/java/Inflearn/security/jwt/controller/RestApiController.java
@@ -1,11 +1,22 @@
 package Inflearn.security.jwt.controller;
 
+import Inflearn.security.jwt.model.User;
+import Inflearn.security.jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class RestApiController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @GetMapping("home")
     public String home() {
@@ -15,5 +26,20 @@ public class RestApiController {
     @PostMapping("token")
     public String token() {
         return "<h1>token</h1>";
+    }
+
+    @GetMapping("admin/users")
+    public List<User> users() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping("join")
+    public String join(@RequestBody User user) {
+        userRepository.save(User.builder()
+                .username(user.getUsername())
+                .password(bCryptPasswordEncoder.encode(user.getPassword()))
+                .roles("ROLE_USER")
+                .build());
+        return "회원가입완료";
     }
 }
